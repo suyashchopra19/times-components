34e5c18849e9a719266e1f230140c06132138fd3
Andy Raines <andrew.raines@news.co.uk>
2018-09-19 13:25:59 +0100

    Only launch google one-click at the bottom of the articles

--- packages/article/src/article.web.js
+++ packages/article/src/article.web.js
@@ -107,68 +107,99 @@ const renderArticle = (
 };
 
 class ArticlePage extends Component {
-  componentDidMount() {
-    window.onGoogleYoloLoad = googleyolo => {
-      const hintPromise = googleyolo.hint({
-        supportedAuthMethods: [
-          "https://accounts.google.com"
-        ],
-        supportedIdTokenProviders: [
-          {
-            uri: "https://accounts.google.com",
-            clientId: "951483460818-ec6ve8477lnok1k912nb0arub0bht5hu.apps.googleusercontent.com"
-          }
-        ]
-      });
+  constructor(props) {
+    super(props);
 
-      hintPromise.then((credential) => {
-        if (credential.idToken) {
-          // Send the token to your auth backend.
-          fetch("/authorize", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json; charset=utf-8",
-            },
-            body: JSON.stringify({token: credential.idToken}),
-          })
-          .then(response => response.json())
-          .then(data => {
-              console.log(data)
-              window.localStorage.setItem("authToken", data.authToken);
-              this.props.refetch();
+    if (typeof window === "undefined" || !("IntersectionObserver" in window)) {
+      return;
+    }
+
+    const options = {
+      rootMargin: "10px",
+      threshold: 0.5
+    };
+    this.showingLogin = false;
+
+    this.observer = new window.IntersectionObserver(
+      this.handleObservation.bind(this),
+      options
+    );
+  }
+
+  handleObservation(entries) {
+    entries.forEach(({ target, intersectionRatio }) => {
+      if (intersectionRatio >= 0.5 && !this.showingLogin) {
+        this.showingLogin = true;
+        const hintPromise = googleyolo.hint({
+          supportedAuthMethods: [
+            "https://accounts.google.com"
+          ],
+          supportedIdTokenProviders: [
+            {
+              uri: "https://accounts.google.com",
+              clientId: "951483460818-ec6ve8477lnok1k912nb0arub0bht5hu.apps.googleusercontent.com"
+            }
+          ]
+        });
+
+        hintPromise.then((credential) => {
+          if (credential.idToken) {
+            // Send the token to your auth backend.
+            fetch("/authorize", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json; charset=utf-8",
+              },
+              body: JSON.stringify({token: credential.idToken}),
             })
-          .catch(error => console.error(error));
-        }
-      }, (error) => {
-        switch (error.type) {
-          case "userCanceled":
-            // The user closed the hint selector. Depending on the desired UX,
-            // request manual sign up or do nothing.
-            break;
-          case "noCredentialsAvailable":
-            // No hint available for the session. Depending on the desired UX,
-            // request manual sign up or do nothing.
-            break;
-          case "requestFailed":
-            // The request failed, most likely because of a timeout.
-            // You can retry another time if necessary.
-            break;
-          case "operationCanceled":
-            // The operation was programmatically canceled, do nothing.
-            break;
-          case "illegalConcurrentRequest":
-            // Another operation is pending, this one was aborted.
-            break;
-          case "initializationError":
-            // Failed to initialize. Refer to error.message for debugging.
-            break;
-          case "configurationError":
-            // Configuration error. Refer to error.message for debugging.
-            break;
-          default:
-            // Unknown error, do nothing.
-        }
-      });
+            .then(response => response.json())
+            .then(data => {
+                console.log(data)
+                window.localStorage.setItem("authToken", data.authToken);
+                this.props.refetch();
+              })
+            .catch(error => console.error(error));
+          }
+        }, (error) => {
+          switch (error.type) {
+            case "userCanceled":
+              // The user closed the hint selector. Depending on the desired UX,
+              // request manual sign up or do nothing.
+              break;
+            case "noCredentialsAvailable":
+              // No hint available for the session. Depending on the desired UX,
+              // request manual sign up or do nothing.
+              break;
+            case "requestFailed":
+              // The request failed, most likely because of a timeout.
+              // You can retry another time if necessary.
+              break;
+            case "operationCanceled":
+              // The operation was programmatically canceled, do nothing.
+              break;
+            case "illegalConcurrentRequest":
+              // Another operation is pending, this one was aborted.
+              break;
+            case "initializationError":
+              // Failed to initialize. Refer to error.message for debugging.
+              break;
+            case "configurationError":
+              // Configuration error. Refer to error.message for debugging.
+              break;
+            default:
+              // Unknown error, do nothing.
+          }
+        });
+      }
+    });
+  }
+
+  componentDidMount() {
+    this.observer.observe(document.getElementById("ArticleTopics"));
+    console.log(document.getElementById("ArticleTopics"));
+
+    window.onGoogleYoloLoad = googleyolo => {
+      this.googleYolo = googleyolo;
     };
   }
 

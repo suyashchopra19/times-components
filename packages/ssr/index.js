af684566f7b6d6b0129767326c6f3311534434a4
James Miller <james@james-miller.co.uk>
2018-09-19 14:47:40 +0100

    spike: Push notifications

--- packages/ssr/index.js
+++ packages/ssr/index.js
@@ -10,6 +10,12 @@ const getData = require("./get-data");
 const article = require("./article");
 const authorProfile = require("./author-profile");
 const topic = require("./topic");
+const request = require("request");
+// const firebase = require("firebase-admin");
+
+// firebase.initializeApp({
+//   messagingSenderId: "56468674053"
+// });
 
 const port = 3000;
 const server = express();
@@ -50,6 +56,53 @@ const makeHtml = (
           </body>
           <script src="/vendor.bundle.js"></script>
           <script src="/${bundleName}.bundle.js"></script>
+          <script src="https://www.gstatic.com/firebasejs/5.5.0/firebase.js"></script>
+          <script src="/firebase-messaging-sw.js"></script>
+          <script>
+            // Initialize Firebase
+            var config = {
+              apiKey: "AIzaSyAd5erE9Vgt0vPyQs8P9Yve8lxp_jU9BoQ",
+              authDomain: "hackathon-c5714.firebaseapp.com",
+              databaseURL: "https://hackathon-c5714.firebaseio.com",
+              projectId: "hackathon-c5714",
+              storageBucket: "hackathon-c5714.appspot.com",
+              messagingSenderId: "56468674053"
+            };
+
+            window.subscribeNotification = function() {
+              firebase.initializeApp(config);
+
+              navigator.serviceWorker
+              .register('/firebase-messaging-sw.js')
+              .then((registration) => {
+                firebase.messaging().useServiceWorker(registration);
+              });
+
+              const messaging = firebase.messaging();
+              messaging
+                  .requestPermission()
+                  .then(() => {
+                    console.log("Have Permission");
+                    return messaging.getToken();
+                  })
+                  .then(token => {
+                    console.log("FCM Token:", token);
+                    console.log(JSON.stringify({token}))
+                    fetch("/push-notification/" + token);
+                    //you probably want to send your new found FCM token to the
+                    //application server so that they can send any push
+                    //notification to you.
+                  })
+                  .catch(error => {
+                    if (error.code === "messaging/permission-blocked") {
+                        console.log("Please Unblock Notification Request Manually");
+                    } else {
+                        console.log("Error Occurred", error);
+                    }
+                    });
+            }
+
+          </script>
         </html>
       `;
 
@@ -113,6 +166,40 @@ server.get("/topic/:slug", (req, res) => {
   );
 });
 
+server.get("/push-notification/:token", (req, res) => {
+  const{ params: { token }} = req;
+  global.james = token;
+  console.log(token)
+});
+
+server.get("/push-notification-send", (req, res) => {
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "key=AAAADSXLlgU:APA91bFdYony5xU4ovErO_HAHg-2w9HxB0L8jZ3haoTqg7YftoRX4-xFW5XfrdOeots5W9MZM7QdTCNefK6S7iWYNkQMwYMXIE7wiBCbFOEO3VqOpiPsW0gVioP2biNvMTA4Q8e1TqP2"
+    },
+    body: {
+      notification: {
+        title: "Britain set for tough new curbs on low‑skilled immigrants",
+        body: "Theresa May’s plans for a tough new immigration regime were given a boost yesterday after a key report called for an end to low-skilled migration from the EU after Brexit.",
+        click_action: "https://www.thetimes.co.uk/article/no-need-for-low-skilled-workers-after-brexit-says-migration-report-jhvsj87b2",
+        icon: "http://localhost:3000/images/times-192.png"
+      },
+      to: global.james
+    },
+    json: true
+  }
+
+  request('https://fcm.googleapis.com/fcm/send', options, (err, response, body) => {
+    console.log(options)
+    if(err){ res.send({"error": err})}
+    res.send(body);
+  });
+
+})
+
 server.use(express.static("dist"));
+server.use(express.static("public"));
 
 server.listen(port, () => console.log(`Serving at http://localhost:${port}`));
